f551af194382cd55e76b1fee1f5b3076b3f61358
iCodeIX <sherwincarlosph@gmail.com>
2025-06-04 12:46:28 +0800

    env changed

--- client/src/Components/utils/axios.jsx
+++ client/src/Components/utils/axios.jsx
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 // const API = axios.create({ baseURL: 'http://localhost:5000/api' });
 
-const API = axios.create({ baseURL: import.meta.env.VITE_API_URL });
+const API = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+});
 
 // Add token to request
 API.interceptors.request.use((config) => {
